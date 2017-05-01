9a79034a41f8677e33c0718215161f8d778aff01
Ryan Caulfield <caulfield.16@osu.edu>
2017-05-01 14:53:14 -0400

    Added 1D scattering stuff to the hamiltonian class. It still needs to be tested though.

--- Hamiltonian.cpp
+++ Hamiltonian.cpp
@@ -6,7 +6,8 @@
 //    Programmer: Ryan Caulfield Caulfield.16@osu.edu
 //
 //    To Do List:
-//      1 - Implement the other construct matrix functions
+//      1 - make set_element more elegant so it can be used in
+//          construct_scattering_matrix
 ///////////////////////////////////////////////////////////////////////////////
 
 //Includes
@@ -45,7 +46,6 @@ Hamiltonian::Hamiltonian (double Rmin, double Rmax, double h, const char potenti
     kpotential = potential;
     construct_localKmatrix();
   }
-  //solve_eigensystem();
 }
 
 //Constructor for an x-space nonlocal potential
@@ -67,7 +67,32 @@ Hamiltonian::Hamiltonian (double Rmin, double Rmax, double h, const char potenti
     knonLocalPotential = potential;
     construct_nonlocalKmatrix();
   }
-  //solve_eigensystem();
+}
+
+//Constructor for 1-D scattering
+Hamiltonian::Hamiltonian (double energy, double Rmin, double Rmax, double h,
+  const char potential_type,
+  std::complex<double>(*potential)(double x, void *params), void *params)
+{
+  //First set all the important attributes
+  left_boundary = Rmin;
+  right_boundary = Rmax;
+  step_size = h;
+  dimension = (Rmax - Rmin) / h;
+  parameters = params;
+  initial_state_energy = energy;
+  if(potential_type == 'x')
+  {
+    xpotential = potential;
+  }
+  if(potential_type == 'k')
+  {
+    kpotential = potential;
+  }
+  //Now construct (E - H_0 + iepsilon)^(-1)V
+  construct_scattering_matrix();
+  construct_initial_wf();
+  scattered_wf = scattering_matrix * initial_wf;
 }
 
 void Hamiltonian::construct_localXmatrix()
@@ -148,6 +173,56 @@ void Hamiltonian::construct_nonlocalKmatrix()
   }
 }
 
+void Hamiltonian::construct_scattering_matrix()
+{
+  scattering_matrix = cx_mat(dimension, dimension);
+  //First construct (E - H_0 + iepsilon)
+  cx_mat temp_matrix1;
+  temp_matrix1 = cx_mat(dimension, dimension);
+  for(int i = 1; i <= dimension; i++){
+    for(int j = 1; j <= dimension; j++){
+      if(i==j){
+        temp_matrix1(i-1,j-1) = -2.0/step_size + initial_state_energy + 1i*10e-8;
+      }
+      else if(i==j-1){
+        temp_matrix1(i-1,j-1) =  1.0 / step_size;
+      }
+      else if(i==j+1){
+        temp_matrix1(i-1,j-1) =  1.0 / step_size;
+      }
+      else{
+        temp_matrix1(i-1,j-1) =  0;
+      }
+    }
+  }
+  //Now construct V as a matrix.
+  cx_mat temp_matrix2;
+  temp_matrix2 = cx_mat(dimension, dimension);
+  for(int i = 1; i <= dimension; i++){
+    for(int j = 1; j <= dimension; j++){
+      double x = left_boundary + double(i) * step_size;
+      if(i==j){
+        temp_matrix2(i-1,j-1) = xpotential(x, parameters);
+      }
+      else{
+        temp_matrix2(i-1,j-1) =  0;
+      }
+    }
+  }
+  scattering_matrix = inv(temp_matrix1) * temp_matrix2;//(E-H_0+iepsilon)^(-1)*V
+}
+
+void Hamiltonian::construct_initial_wf()
+{
+  initial_wf = cx_vec(dimension);
+  double k = sqrt(initial_state_energy);
+  for(int i = 1; i < dimension; i++){
+    double x = left_boundary + double(i) * step_size;
+    complex<double> ikx = 1i * k * x;
+    initial_wf(i) = exp(ikx);
+  }
+}
+
 void Hamiltonian::solve_eigensystem()
 {
   eig_sym(eigenvalues, eigenvectors, Hmatrix);
@@ -185,4 +260,34 @@ std::complex<double> Hamiltonian::get_eigenvalue(const int i)
 std::complex<double> Hamiltonian::get_eigenvector(const int i, const int j)
 {
   return eigenvectors (i-1, j-1);
+}
+
+double Hamiltonian::calculate_T()
+{
+  complex<double> t = 0;
+  double k = sqrt(initial_state_energy);
+
+  for(int i = 1; i < dimension; i++)
+  {
+    double x = left_boundary + double(i) * step_size;
+    complex<double> ikx = 1i * k * x;
+    t += exp(ikx)*scattered_wf(i);
+  }
+  t /= (right_boundary - left_boundary);
+  return(norm(t));
+}
+
+double Hamiltonian::calculate_R()
+{
+  complex<double> r = 0;
+  double k = sqrt(initial_state_energy);
+
+  for(int i = 1; i < dimension; i++)
+  {
+    double x = left_boundary + double(i) * step_size;
+    complex<double> ikx = 1i * k * x;
+    r += exp(ikx)*scattered_wf(i);
+  }
+  r /= (right_boundary - left_boundary);
+  return(norm(r));
 }
--- Hamiltonian.h
+++ Hamiltonian.h
@@ -1,11 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
 //    File: Hamiltonian.h
-//    Description: Header file for Hamiltonian.cpp. This contains information
-//                 about potentials and finite difference matrix forms. The
-//                 discretization is done with uniform spacing for a step
-//                 size which is also stored as a parameter. Using these methods
-//                 we can solve for eigenstates and eigenvalues.
+//    Description: Header file for Hamiltonian.cpp.
 //
 //    Programmer: Ryan Caulfield Caulfield.16@osu.edu
 //
@@ -36,15 +32,28 @@ class Hamiltonian
     //potential in x-space or k-space. The potential_type should be "x" or "k".
     Hamiltonian (double Rmin, double Rmax, double h, const char potential_type,
       std::complex<double>(*potential)(double x1, double x2, void *params), void *params);
+    //This constructor will be for 1-D scattering using the born approximation.
+    //It constructs (E - H_0 + iepsilon)^(-1)V and has it act on a initial
+    //plane wave state. Just as in the previous, the potential type should be
+    //"x" or "k"
+    Hamiltonian (double energy, double Rmin, double Rmax, double h,
+      const char potential_type,
+      std::complex<double>(*potential)(double x, void *params), void *params);
 
     ~Hamiltonian ();  // destructor
 
     //These construct the hamiltonian matrix for x or k both local and nonlocal.
-    //The utilize dirichlet boundary conditions
+    //These utilize dirichlet boundary conditions.
     void construct_localXmatrix();
     void construct_localKmatrix();
     void construct_nonlocalXmatrix();
     void construct_nonlocalKmatrix();
+
+    //Constructs the scattering matrix for 1D scattering.
+    void construct_scattering_matrix();
+    //constructs a plane wave initial wave function.
+    //used in 1D scattering.
+    void construct_initial_wf();
 
     //This solves for the eigenvectors and eigenvalues. After, the can be
     //accesed with the appropriate getter function.
@@ -56,16 +65,24 @@ class Hamiltonian
     std::complex<double> get_element(const int i, const int j);
     std::complex<double> get_eigenvalue(const int i);
     std::complex<double> get_eigenvector(const int i, const int j);
+    double calculate_T();
+    double calculate_R();
 
   private:
     double left_boundary;     // The minimum value of x
-    double right_boundary;   // maximum value of x
+    double right_boundary;    // maximum value of x
     int dimension;            // Dimensionality
     double step_size;         // The step size used for FD approximation of
                               // the derivative.
-    arma::cx_mat Hmatrix;        // The matrix form of the Hamiltonian
+    double initial_state_energy;//The energy of the initial wave function used
+                              //in 1D scattering
+    arma::cx_mat Hmatrix;     // The matrix form of the Hamiltonian
+    arma::cx_mat scattering_matrix; // (E - H_0 + iepsilon)^(-1)V
+                                    // used for 1D scattering
     arma::vec eigenvalues;    // vector of eigenvalues
-    arma::cx_mat eigenvectors;   // matrix of eigenvectors
+    arma::cx_mat eigenvectors;// matrix of eigenvectors
+    arma::cx_vec initial_wf;  // The initial wave function used in 1D scattering
+    arma::cx_vec scattered_wf;// The scattered wave function used in 1D scattering
     std::complex<double>(*xpotential)(double x, void *params);
                               //potential in xspace
     std::complex<double>(*kpotential)(double k, void *params);
