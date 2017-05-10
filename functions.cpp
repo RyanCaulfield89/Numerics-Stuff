712a9f5d5e3345cea384c2f4250efa6d43f831a3
Ryan Caulfield <caulfield.16@osu.edu>
2017-05-10 15:27:41 -0400

    added some stuff to functions

--- functions.cpp
+++ functions.cpp
@@ -18,46 +18,62 @@ using namespace std;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-std::complex<double> coulomb_potential(double x, void *params)
+complex<double> coulomb_potential(double x, void *params)
 {
-  std::complex<double> charge;
-  charge = *(std::complex<double> *) params;
-  std::complex<double> return_value;
+  complex<double> charge;
+  charge = *(complex<double> *) params;
+  complex<double> return_value;
   return_value = charge / x;
   return (return_value);
 }
 
-std::complex<double> harmonic_potential(double x, void *params)
+complex<double> harmonic_potential(double x, void *params)
 {
-  std::complex<double> k;
-  k = *(std::complex<double> *) params;
-  std::complex<double> return_value = 0.5*k*x*x;
+  complex<double> k;
+  k = *(complex<double> *) params;
+  complex<double> return_value = 0.5*k*x*x;
   return (return_value);
 }
 
-std::complex<double> nonlocal_coulomb_potential(double x, double y, void *params)
+complex<double> nonlocal_coulomb_potential(double x, double y, void *params)
 {
-  std::complex<double> beta;
-  beta = *(std::complex<double> *) params;
-  std::complex<double> return_value;
+  complex<double> beta;
+  beta = *(complex<double> *) params;
+  complex<double> return_value;
   return_value =  1 / fabs(x - y) * exp(-(x - y)*(x - y) / beta);
   return (return_value);
 }
 
-std::complex<double> nonlocal_harmonic_potential(double x, double y, void *params)
+complex<double> nonlocal_harmonic_potential(double x, double y, void *params)
 {
-  std::complex<double> beta;
-  beta = *(std::complex<double> *) params;
-  std::complex<double> return_value;
+  complex<double> beta;
+  beta = *(complex<double> *) params;
+  complex<double> return_value;
   return_value = (x - y)*(x - y) * exp(-(x - y)*(x - y) / beta);
   return (return_value);
 }
 
-std::complex<double> nonlocal_potential(double x, double y, void *params)
+complex<double> nonlocal_potential(double x, double y, void *params)
 {
-  std::complex<double> beta;
-  beta = *(std::complex<double> *) params;
-  std::complex<double> return_value;
+  complex<double> beta;
+  beta = *(complex<double> *) params;
+  complex<double> return_value;
   return_value = - (x + y) * exp(-(abs(x - y) - 1)*(abs(x - y) - 1) / beta);
   return (return_value);
+}
+
+complex<double> seperable_nonlocal_potential(double x, double y, void *params){
+  complex<double> beta;
+  beta = *(complex<double> *) params;
+  complex<double> return_value;
+  return_value = x*x*exp(-beta*y*y);
+  return return_value;
+}
+complex<double> quadratic_polynomial(double x, void *params){
+  complex<double> a = ((quadratic_parameters *) params)->a;
+  complex<double> b = ((quadratic_parameters *) params)->b;
+  complex<double> c = ((quadratic_parameters *) params)->c;
+  complex<double> return_value;
+  return_value = a*x*x + b*x + c;
+  return return_value;
 }
--- functions.h
+++ functions.h
@@ -2,7 +2,7 @@
 //
 //    File: functions.h
 //    Description: Header file for functions.h
-//                 This is a collection of functions that we care about.
+//                 This is a collection of functions that we maybe care about.
 //
 //    Programmer: Ryan Caulfield Caulfield.16@osu.edu
 //
@@ -15,12 +15,24 @@
 #define FUNCTIONS_H //To avoid including this file twice
 //Headers
 
-//Coulomb potential. The parameter pointer needs to have an attribute q, which
-//is the charge of the particle in units of 4*Pi*epsilon_0.
+//structures for the function parameters
+
+//For quadratic_polynomial
+typedef struct
+{
+  std::complex<double> a;
+  std::complex<double> b;
+  std::complex<double> c;
+}
+quadratic_parameters;
+
+///////////////////////////////////////////////////////////////////////////////
 std::complex<double> coulomb_potential(double x, void *params);
 std::complex<double> harmonic_potential(double x, void *params);
 std::complex<double> nonlocal_coulomb_potential(double x, double y, void *params);
 std::complex<double> nonlocal_harmonic_potential(double x, double y, void *params);
 std::complex<double> nonlocal_potential(double x, double y, void *params);
+std::complex<double> seperable_nonlocal_potential(double x, double y, void *params);
+std::complex<double> quadratic_polynomial(double x, void *params);
 
 #endif
