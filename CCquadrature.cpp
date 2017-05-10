209737f952f608ffd028427121f12b9b66ff519b
Ryan Caulfield <caulfield.16@osu.edu>
2017-05-10 12:58:28 -0400

    added some stuff to CCquadrature

--- CCquadrature.cpp
+++ CCquadrature.cpp
@@ -6,7 +6,10 @@
 //    Programmer: Ryan Caulfield Caulfield.16@osu.edu
 //
 //    To Do List:
-//      1 - This needs to be tested
+//      1 - This needs to be tested. I should make an example file tht does a
+//          bunch of stuff. It should include solving an IDE, doing an integral
+//          of a polynomial exactly, doing an integral of another function
+//          accurately and representing a function as a series of polynomials.
 ///////////////////////////////////////////////////////////////////////////////
 
 //Includes
@@ -15,10 +18,17 @@ using namespace arma;
 using namespace std;
 
 ///////////////////////////////////////////////////////////////////////////////
-CCquadrature::CCquadrature(int n, double a, double b){
+CCquadrature::CCquadrature(int n, double a, double b,
+  complex<double>(*my_function)(double x, void *params), void *params){
   numpoints = n;
   left_boundary = a;
   right_boundary = b;
+  integrand = my_function;
+  parameters = params;
+  find_points();
+  find_weights();
+  find_coefficients();
+  construct_diff_matrix();
 }
 
 void CCquadrature::construct_diff_matrix(){
@@ -30,7 +40,7 @@ void CCquadrature::construct_diff_matrix(){
   diff_matrix(numpoints,numpoints) = -(2.0*numpoints + 1.0) / 6.0;
   diff_matrix(0,numpoints) = pow(-1.0,numpoints) / 2.0;
   diff_matrix(numpoints,0) = -pow(-1.0,numpoints) / 2.0;
-  //Now, do the first and last column and row.
+  //Now, do the first and last column and row not including the corners.
   for(int j = 1; j < numpoints; j++){
     diff_matrix(0,j) = 2.0 * pow(-1.0,j) / (1.0 - points(j));
     diff_matrix(numpoints,j) = -2.0 * pow(-1.0,numpoints + j)/(1.0 + points(j));
@@ -68,7 +78,19 @@ void CCquadrature::find_weights(){
   }
 }
 
-double nth_Tchebyshev_polynomial(int n, double x){
+void CCquadrature::find_coefficients(){
+  coefficients = cx_vec(numpoints + 1);
+  for(int i = 0; i <= numpoints; i++){
+    for(int j = 1; j <= numpoints; j++){
+      coefficients(i) += integrand(points(j),params) *
+      nth_Tchebyshev_polynomial(i,points(j));
+    }
+    coefficients(i) *= 2.0/numpoints;
+  }
+  coefficients(0) *= 0.5;
+}
+
+double CCquadrature::nth_Tchebyshev_polynomial(int n, double x){
   if(abs(x)<=1){
     return cos(n * acos(x));
   }
@@ -84,22 +106,30 @@ double nth_Tchebyshev_polynomial(int n, double x){
   }
 }
 
-double nth_Uchebyshev_polynomial(int n, double x){
+double CCquadrature::nth_Uchebyshev_polynomial(int n, double x){
   return (pow(x+sqrt(x*x-1),n+1) - pow(x-sqrt(x*x-1),n+1))/(2*sqrt(x*x-1));
 }
 
-double diff_nth_Tchebyshev_polynomial(int n, double x){
+double CCquadrature::diff_nth_Tchebyshev_polynomial(int n, double x){
   //uses dT_n/dx(x) = nU_n-1(x)
   //identity, look it up
   return n*nth_Uchebyshev_polynomial(n-1, x);
 }
 
-double diff_nth_Uchebyshev_polynomial(int n, double x){
+double CCquadrature::diff_nth_Uchebyshev_polynomial(int n, double x){
   //uses dU_n/dx(x) = ((n+1)*T_n+1(x)-xU_n(x))/(x^2-1)
   //identity, look it up
   return ((n+1)*nth_Tchebyshev_polynomial(n+1,x)
   - x*nth_Uchebyshev_polynomial(n,x))
   / (x*x - 1);
+}
+
+double CCquadrature::evaluate_integral(){
+  double sum = 0;
+  for(int i = 0; i <= numpoints; i++){
+    sum += integrand(points(i),params) * weights(i);
+  }
+  return sum;
 }
 
 int CCquadrature::get_numpoints(){
@@ -114,14 +144,18 @@ double CCquadrature::get_right_boundary(){
   return right_boundary;
 }
 
-arma::vec CCquadrature::get_points(){
+vec CCquadrature::get_points(){
   return points;
 }
 
-arma::vec CCquadrature::get_weights(){
+vec CCquadrature::get_weights(){
   return weights;
 }
 
-arma::mat CCquadrature::get_diff_matrix(){
+cx_vec CCquadrature::get_coefficients(){
+  return coefficients;
+}
+
+mat CCquadrature::get_diff_matrix(){
   return diff_matrix;
 }
--- CCquadrature.h
+++ CCquadrature.h
@@ -18,7 +18,8 @@ class CCquadrature
 {
   public:
     //Constructs the object and initializes the # of points and the boundaries.
-    CCquadrature(int n, double a, double b);
+    CCquadrature(int n, double a, double b,
+      std::complex<double>(*my_function)(double x, void *params), void *params);
     //Destructor.
     ~CCquadrature();
 
@@ -28,6 +29,8 @@ class CCquadrature
     void find_points();
     //This finds the weight for each node.
     void find_weights();
+    //This find the expansion coefficients for the chebyshev interpolation
+    void find_coefficients();
     //Calculates the nth chebyshev polynomial of the 1st kind at x.
     double nth_Tchebyshev_polynomial(int n, double x);
     //Calculates the nth chebyshev polynomial of the 2nd kind at x.
@@ -38,7 +41,8 @@ class CCquadrature
     //Calculates the derivative of the nth chebyshev polynomial of
     //the 2nd kind at x.
     double diff_nth_Uchebyshev_polynomial(int n, double x);
-
+    //This evaluated the integral using the quadrature rule.
+    double evaluate_integral();
 
     //Getters for the important stuff.
     int get_numpoints();
@@ -46,6 +50,7 @@ class CCquadrature
     double get_right_boundary();
     arma::vec get_points();
     arma::vec get_weights();
+    arma::cx_vec get_coefficients();
     arma::mat get_diff_matrix();
 
   private:
@@ -54,7 +59,11 @@ class CCquadrature
     double right_boundary;    //Right boundary or upper bound
     arma::vec points;         //Quadrature points
     arma::vec weights;        //Quadrature weights
+    arma::cx_vec coefficients;//Coefficients in the interpolation expansion
     arma::mat diff_matrix;    //Differentiation matrix
+    std::complex<double>(*integrand)(double x, void *params);
+                              //The function to be integrated or interpolated
+    void *parameters;         //parameters for the potentials
 }
 
 #endif
